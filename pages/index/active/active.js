4a81afec60f90b770f4782f162f4a135189775d6
于世芳 <1060173122@qq.com>
2018-11-09 18:53:46 +0800

    119

--- pages/index/active/active.js
+++ pages/index/active/active.js
@@ -1,93 +1,95 @@
+// pages/index/active/active.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    cellList:[
-      {
-        name:'注册会员',
-        icon:'icon-zhuce',
-        path:'./reg/reg'
-      },
-      {
-        name: '注册会员',
-        icon: 'icon-zhuce',
-        path: './active/active'
-      },
-      {
-        name: '注册会员',
-        icon: 'icon-zhuce',
-        path: './reg/reg'
-      },
-       {
-        name: '注册会员',
-        icon: 'icon-zhuce',
-        path: './reg/reg'
-      }
-    ]
+    array: ['美国', '中国', '巴西', '日本'],
+    index:0,
+    number:1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   },
-  //九宫格页面跳转
-  jump:function(e){
+  bindPickerChange: function (e) {
+    this.setData({
+      index: e.detail.value
+    })
+  },
+  changeNumber:function(e){
     console.log(e);
-    wx.navigateTo({
-      url: e.currentTarget.dataset.path,
+    if (e.currentTarget.dataset.type=='add'){
+        this.setData({
+          number: this.data.number+1
+        })
+    } else if (e.currentTarget.dataset.type == 'jian'){
+      this.setData({
+        number: this.data.number==1?1:this.data.number - 1
+      })
+    }
+  },
+  number:function(e){
+    this.setData({
+      number: Number(e.detail.value)
+    })
+  },
+  remarks:function(e){
+    this.setData({
+      remarks:e.detail.value
     })
   }
 })
